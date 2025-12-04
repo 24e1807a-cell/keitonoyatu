e035ee9e3c20ef8e80f309399935c45ca84c23f9
24e1807a-cell <24e1807a@shinshu-u.ac.jp>
2025-12-04 14:16:40 +0900

    Update joho.py

--- joho.py
+++ joho.py
@@ -131,27 +131,24 @@ for song in songs:
     # キーワードに一致したら表示
     if any(k in title for k in keywords):
 
+     for song in song_list:
        st.markdown(f"""
-<div style="
-background-color: rgba(0,0,0,0.6);
-padding: 20px;
-border-radius: 20px;
-margin-bottom: 20px;
-color: white;
-">
-<h3>🎵 {song['trackName']}</h3>
-<p>🎤 {song['artistName']}</p>
-<p>{make_description(song)}</p>
-</div>
-""", unsafe_allow_html=True)
+    <div style="
+    background-color: rgba(0,0,0,0.5);
+    padding: 20px;
+    border-radius: 20px;
+    margin-bottom: 20px;
+    color: white;
+    ">
+    <h3>🎵 {song['trackName']}</h3>
+    <p>🎤 {song['artistName']}</p>
+    <p>{make_description(song)}</p>
+    </div>
+    """, unsafe_allow_html=True)
 
-
-    displayed.add(title)
     count += 1
-
-        # 5曲出たら終了
-    if count == MAX_SONGS:
-            break
+    if count >= 5:
+        break
 
 
 # ---------- 足りなかったときの保険 ----------
